6985166f7d40f802bf1623c2a2da900abe98d8f0
Ayush Tamang <tamangayush052@gmail.com>
2024-06-25 14:28:51 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,23 @@
+// declaring variables
+let a =20
+var b =20
+const c =20
+
+// array
+const names = ['Ayush','Subin','Chetan','Sujal','Ninam']
+names.push('hello') // add value to object
+console.log(names)
+names.pop()
+names.unshift('hello') 
+names.shift()
+names.length
+
+
+// Object
+const info = {
+    name: 'Ayush',
+    age: 19,
+    address : "Dharan"
+}
+info.faculty = "BCA"
+console.log(info)
